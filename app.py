3c8f2a0b1c5e47db14e81741e1867ed3c9671e94
Lindsey G <lgiron84@gmail.com>
2021-01-21 10:06:52 -0800

    adding app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect
+
+# Initiate Flask app
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+
+@app.route("/api/v1.0/calendar")
+def calendar():
+
+@app.route("/api/v1.0/map")
+def map():
+
+@app.route("/api/v1.0/scatter")
+def scatter():
+
+@app.route("/api/v1.0/")
+
+
+
+# Run app
+if __name__ == "__main__":
+    app.run(debug=True) 
