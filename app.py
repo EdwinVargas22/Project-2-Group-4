9d674b5910fa02d4660b3f4908a721b2917036ac
Lindsey G <lgiron84@gmail.com>
2021-01-24 17:14:26 -0800

    flask app.py is reading in data from postgres. we have successfully return jsonify data

--- app.py
+++ app.py
@@ -11,27 +11,9 @@ from config import pg_password
 #####################################################
 # Database Setup
 #####################################################
-
-
 # Create connection string to postgres
 connection_string = f"postgres:{pg_password}@localhost:5432/covid_mask_effect_db"
 engine = create_engine(f'postgresql://{connection_string}')
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = engine
-db = SQLAlchemy(app)
-
-@app.route('/', methods=['POST', 'GET'])
-def testfn():
-
-    # Get request
-    if request.method == 'GET':
-        covid_data = cod
-        return jsonify(message)
-
-    if request.method == 'POST':
-        print(request.get_json())
-        return 'Sucess', 200
 
 # Reflect database into a new model using Base = automap()
 Base = automap_base()
@@ -56,22 +38,23 @@ def home_page():
     # Create connection
     session = Session(engine)
     # california_data
-    cali_counties = session.query(cali_data.county).all()
+    # cali_counties = session.query(cali_data.county).all()
     cali_cases = session.query(cali_data.cases).all()
     cali_dates = session.query(cali_data.date).all()
+    # dates_total = session.quesry(cali)
 
     # nocal_data
     nocal_counties = session.query(nocal_data.county).all()
     nocal_cases = session.query(nocal_data.cases).all()
-    nocal_dates = session.query(nocal_data.date).all()
+    # nocal_dates = session.query(nocal_data.date).all()
 
     # socal_data
     socal_counties = session.query(socal_data.county).all()
     socal_cases = session.query(socal_data.cases).all()
-    socal_dates = session.query(socal_data.date).all()
+    # socal_dates = session.query(socal_data.date).all()
 
-    # return jsonify({"North Cali County": nocal_counties}, {"North Cali Cases": nocal_cases}, {"North Cali Dates": nocal_dates}, {"So Cal County": socal_counties}, {" So Cal Cases": socal_cases}, {"So Cal Dates": socal_dates})
-    return jsonify({"county" : socal_dates})
+    return jsonify({"Cali Cases": cali_cases, "Cali Dates": cali_dates}, {"North Cali County": nocal_counties, "North Cali Cases": nocal_cases}, {"So Cal County": socal_counties, " So Cal Cases": socal_cases})
+
 # @app.route('/test', methods=['GET', 'POST'])
 
 # def homepage():
