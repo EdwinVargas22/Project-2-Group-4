014015197e65eee3706d7bdca5a4c0cc7e422879
Taylor Arnold <taylorj.arnold2013@hotmail.com>
2021-01-24 20:28:15 -0800

    update

--- templates/static/js/logic3.js
+++ templates/static/js/logic3.js
@@ -1,69 +1,54 @@
-$(document).ready(function () {
-    var newArray = [];
-    d3.csv("", function(error, data) {// with header..
-        data.forEach(function(d) {
-                newArray.push(d);
-        });
-
-        });
-        d3.csv("", function(error, data2) {// without header..
-            data2.forEach(function(d2) {
-                    newArray.push(d2);
-            });
-
-            });
-        console.log(newArray);
-    });
-
-    var county = unpack(rows, 'county'),
-        cases = unpack(rows, 'cases'),
-        northern_cali_fips = unpack(rows, 'northern_cali_fips'),
-        southern_cali_fips = unpack(rows, 'southern_cali_fips'),
-        color = [,"rgb(255,65,54)","rgb(133,20,75)","rgb(255,133,27)","lightgrey"],
-        citySize = [],
-        hoverText = [],
-        scale = 50000;
-
-    for ( var i = 0 ; i < cityPop.length; i++) {
-        var currentSize = cityPop[i] / scale;
-        var currentText = cityName[i] + " pop: " + cityPop[i];
-        citySize.push(currentSize);
-        hoverText.push(currentText);
-    }
-
-    var data = [{
-        type: 'scattergeo',
-        locationmode: 'USA-states',
-        lat: cityLat,
-        lon: cityLon,
-        hoverinfo: 'text',
-        text: hoverText,
-        marker: {
-            size: citySize,
-            line: {
-                color: 'black',
-                width: 2
-            },
-        }
-    }];
-
-    var layout = {
-        title: '2020 California COVID-19 Cases - Northern v. Southern ',
-        showlegend: false,
-        geo: {
-            scope: 'usa',
-            projection: {
-                type: 'albers usa'
-            },
-            showland: true,
-            landcolor: 'rgb(217, 217, 217)',
-            subunitwidth: 1,
-            countrywidth: 1,
-            subunitcolor: 'rgb(255,255,255)',
-            countrycolor: 'rgb(255,255,255)'
-        },
-    };
-
-    Plotly.newPlot("myDiv", data, layout, {showLink: false});
-
-});
+chart = {
+    const svg = d3.create("svg")
+        .attr("viewBox", [0, 0, 975, 610]);
+  
+    svg.append("path")
+        .datum(topojson.feature(us, us.objects.nation))
+        .attr("fill", "#ddd")
+        .attr("d", path);
+  
+    svg.append("path")
+        .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
+        .attr("fill", "none")
+        .attr("stroke", "white")
+        .attr("stroke-linejoin", "round")
+        .attr("d", path);
+  
+    const legend = svg.append("g")
+        .attr("fill", "#777")
+        .attr("transform", "translate(915,608)")
+        .attr("text-anchor", "middle")
+        .style("font", "10px sans-serif")
+      .selectAll("g")
+        .data(radius.ticks(4).slice(1))
+      .join("g");
+  
+    legend.append("circle")
+        .attr("fill", "none")
+        .attr("stroke", "#ccc")
+        .attr("cy", d => -radius(d))
+        .attr("r", radius);
+  
+    legend.append("text")
+        .attr("y", d => -2 * radius(d))
+        .attr("dy", "1.3em")
+        .text(radius.tickFormat(4, "s"));
+  
+    svg.append("g")
+        .attr("fill", "brown")
+        .attr("fill-opacity", 0.5)
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 0.5)
+      .selectAll("circle")
+      .data(data
+          .filter(d => d.position)
+          .sort((a, b) => d3.descending(a.value, b.value)))
+      .join("circle")
+        .attr("transform", d => `translate(${d.position})`)
+        .attr("r", d => radius(d.value))
+      .append("title")
+        .text(d => `${d.title}
+  ${format(d.value)}`);
+  
+    return svg.node();
+  }
