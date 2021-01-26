360a25c2e98a5b54b5a650194069931d350452a2
Lindsey G <lgiron84@gmail.com>
2021-01-26 01:36:12 -0800

    added cali_county table

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-# import dependencies
+##################################################
+#        import dependencies                     #
+##################################################
 import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -9,7 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import pg_password
 
 #####################################################
-# Database Setup
+#               Database Setup                      #
 #####################################################
 # Create connection string to postgres
 connection_string = f"postgres:{pg_password}@localhost:5432/covid_mask_effect_db"
@@ -21,13 +23,14 @@ Base = automap_base()
 # Setup .prepare(engine, reflect=True) to reflect tables
 Base.prepare(engine, reflect=True)
 
-# Save reference to california_data, nocal_data, and socal_data tables
+# Create variables for cali_2020, cali_county, norcal_geo, and socal_geo tables in postgres
 cali_data = Base.classes.cali_2020
+cali_counties = Base.classes.cali_county
 norcal_data = Base.classes.norcal_geo
 socal_data = Base.classes.socal_geo
 
 #######################################################
-# Setup Flask
+#               Setup Flask                           #
 #######################################################
 
 # Initiate Flask app
@@ -35,62 +38,42 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    # Create empty list
-    cali_cases_date = []
-    # Create connection
-    session = Session(engine)
-    # california_data
-    # cali_counties = session.query(cali_data.county).all()
-    cali_cases = session.query(cali_data.cases).all()
-    cali_dates = session.query(cali_data.date).all()
-    # dates_total = session.quesry(cali)
+    welcome = "Welcome! Currently working on the magic that will render this page"
+    return(welcome)
 
-    # nocal_data
-    norcal_counties = session.query(norcal_data.fips).all()
-    norcal_cases = session.query(norcal_data.cases).all()
-    # nocal_dates = session.query(nocal_data.date).all()
-
-    # socal_data
-    socal_counties = session.query(socal_data.fips).all()
-    socal_cases = session.query(socal_data.cases).all()
-    # socal_dates = session.query(socal_data.date).all()
-
-    # return jsonify({"Cali Cases": cali_cases, "Cali Dates": cali_dates}, {"North Cali County": nocal_counties, "North Cali Cases": nocal_cases}, {"So Cal County": socal_counties, " So Cal Cases": socal_cases})
-    return jsonify(cali_dates)
-# @app.route('/test', methods=['GET', 'POST'])
-
-# def homepage():
-
-# @app.route("/api/v1.0/calendar")
-# def calendar():
-#     return 
-
-# @app.route("/api/v1.0/map")
-# def map():
-
-# @app.route("/api/v1.0/scatter")
-# def scatter():
-
-# @app.route("/api/v1.0/")
 #############################################################
-# Endpoints for jsonified data
+#       Create Endpoints to return jsonify data             #
 #############################################################
-# cali_cases_by_date endpoint
+
 @app.route("/api/v3.0/cali_cases_by_date")
 def cali_cases_by_date():
-    # Connect to engine
+    # Connect to engine to connect to postgres database
     session = Session(engine)
  
-    # session.query for all cali_data for cases and date
-    cali_cases = session.query(cali_data.cases).all()
+    # session.query cali_data for cases and date to use in calendar view visualization
     cali_dates = session.query(cali_data.date).all()
+    cali_cases = session.query(cali_data.cases).all()
+
+    # return data with jsonify
+    return jsonify([{"date": cali_dates, "california total cases": cali_cases}])
+
+@app.route("/api/v3.0/cali_cases_by_county")
+def cali_cases_by_date():
+    # Connect to engine to connect to postgres database
+    session = Session(engine)
+ 
+    # session.query for cali_counties for county, cases and date
+    cali_counties = session.query(cali_counties.county).all()
+    cali_county_cases = session.query(cali_counties.cases).all()
+    cali_count_dates = session.query(cali_counties.date).all()
+
     # return data with jsonify
     return jsonify([{"All Cali Cases": cali_cases, "date": cali_dates}])
 
 # nocal_county_cases endpoint
 @app.route("/api/v3.0/nocal_county_cases")
 def nocal_county_cases():
-    # Connect to engine
+    # Connect to engine to connect to postgres database
     session = Session(engine)
     
     # session.query for all nocal_data for fips and cases
@@ -98,6 +81,7 @@ def nocal_county_cases():
     norcal_cases = session.query(norcal_data.cases).all()
     norcal_lat = session.query(norcal_data.latitude).all()
     norcal_long = session.query(norcal_data.longitude).all()
+    norcal_deaths = session.query()
     # return data with jsonify
     return jsonify([{"county": norcal_counties, "cases": norcal_cases, "latitude": norcal_lat, "longitude": norcal_long}])
 
@@ -112,6 +96,7 @@ def socal_county_cases():
     socal_cases = session.query(socal_data.cases).all()
     socal_lat = session.query(socal_data.latitude).all()
     socal_long = session.query(socal_data.longitude).all()
+    socal_deaths = 
     # return data with jsonify
     return jsonify([{"county": socal_counties, "cases": socal_cases,"latitude": socal_lat, "longitude": socal_long}])
 
