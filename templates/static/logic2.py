2c279958eefdc5f49cad30acdb733e092ada89f5
Lindsey G <lgiron84@gmail.com>
2021-01-27 14:22:14 -0800

    scatter plot

--- templates/static/logic2.py
+++ templates/static/logic2.py
@@ -1,54 +1,48 @@
-import os
+import mimetypes
 import plotly.graph_objects as go
 import pandas as pd
-import mimetypes
-import numpy as np
-import datetime
-import plotly.figure_factory as ff
-import plotly.express as px
 
 # deal with mimetype error
 print(mimetypes.guess_extension("application/octet-stream"))
 
 # Read in csv
 cases_deaths = pd.read_csv("../../output/cali_county.csv")
-# df = px.cases_deaths.iris()
 
-# # set up variables
-# dates = cases_deaths['date'].tolist()
-# cases = cases_deaths['cases'].tolist()
-# county = cases_deaths['county'].tolist()
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
+df.head()
 
-fig = px.scatter(cases_deaths, x="county", y="cases", color="county",
-                 size='cases', hover_data=['county'])
+df['text'] = df['name'] + '<br>Population ' + (df['pop']/1e6).astype(str)+' million'
+limits = [(0,2),(3,10),(11,20),(21,50),(50,3000)]
+colors = ["royalblue","crimson","lightseagreen","orange","lightgrey"]
+cities = []
+scale = 5000
+
+fig = go.Figure()
+
+for i in range(len(limits)):
+    lim = limits[i]
+    df_sub = df[lim[0]:lim[1]]
+    fig.add_trace(go.Scattergeo(
+        locationmode = 'USA-states',
+        lon = df_sub['lon'],
+        lat = df_sub['lat'],
+        text = df_sub['text'],
+        marker = dict(
+            size = df_sub['pop']/scale,
+            color = colors[i],
+            line_color='rgb(40,40,40)',
+            line_width=0.5,
+            sizemode = 'area'
+        ),
+        name = '{0} - {1}'.format(lim[0],lim[1])))
+
+fig.update_layout(
+        title_text = '2014 US city populations<br>(Click legend to toggle traces)',
+        showlegend = True,
+        geo = dict(
+            scope = 'usa',
+            landcolor = 'rgb(217, 217, 217)',
+        )
+    )
+
 fig.show()
-
-# z = np.random.poisson(size=(len(cases), len(dates)))
-
-# fig = go.Figure(data=go.Heatmap(
-#         x=dates,
-#         y=cases,
-#         colorscale='Viridis'))
-
-
-# fig.update_layout(
-#     title='COVID-19',
-#     xaxis_nticks=58)
-# fig.show()
-
-# # Create variables for deaths and cases data
-# deaths = cases_deaths['deaths'].tolist()
-# cases = cases_deaths['cases'].tolist()
-# dates = cases_deaths['date'].tolist()
-# print(max(cases))
-# fig = go.Figure(data=[go.Scatter(
-#     x= dates,
-#     y= cases,
-#     mode='markers',
-#     marker=dict(
-#         color=['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-#         size=[40, 60, 80, 100],
-#         showscale=True
-#     )
-# )])
-# fig.show()
--- templates/static/logicScatter.py
+++ templates/static/logicScatter.py
@@ -1,20 +1,15 @@
 import os
-import plotly.graph_objects as go
 import pandas as pd
 import mimetypes
-import numpy as np
-import datetime
-import plotly.figure_factory as ff
 import plotly.express as px
 
 # deal with mimetype error
 print(mimetypes.guess_extension("application/octet-stream"))
 
 # Read in csv
-cases_deaths = pd.read_csv("../../output/cali_county.csv")
+cali_by_counties = pd.read_csv("../../output/cali_county.csv")
 
-cases = cases_deaths['cases']
 
-fig = px.scatter(cases_deaths, x="county", y="cases", color="county",
-                 size=radius_size(cases), hover_data=['county'])
+fig = px.scatter(cali_by_counties, x="county", y="cases", color="county",
+                 size='cases', hover_data=['county'])
 fig.show()
